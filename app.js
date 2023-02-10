b54271a7ed4c5f1842315c1be277ae2ef4503c07
Juliana-GomezU <123768367+Juliana-GomezU@users.noreply.github.com>
2023-02-10 14:00:09 -0500

    quiz, imgs

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-document.addEventListener("DOMcontentLoaded", () => {});
+document.addEventListener("DOMcontentLoaded", () => {
+ 
+  const cardAd = [
+    {
+      name: "66139",
+      img: "images/66139.jpg"
+    },
+    {  
+      name: "Princess E-Girl Fashion (1)",
+      img: "images/Princess E-Girl Fashion (1).jpg"
+    },
+    {
+      name: "megasloooooo",
+      img: "images/megasloooooo.jpg"
+    },
+    {
+      name: "megasloooooo",
+      img: "images/megasloooooo.jpg"
+    },
+  ];
+});
