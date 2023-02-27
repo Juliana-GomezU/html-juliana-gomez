e657b5715cd159f91ad9f8163c74cfdb3fd595e3
Juliana-GomezU <123768367+Juliana-GomezU@users.noreply.github.com>
2023-02-27 12:41:49 -0500

    sesion 3

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 document.addEventListener("DOMcontentLoaded", () => {
  
-  const cardAd = [{name: "66139",img: "images/66139.jpg"},
-    {name: "Princess E-Girl Fashion (1)",img: "images/Princess E-Girl Fashion (1).jpg"};
-    {name: "megasloooooo",img: "images/megasloooooo.jpg"};
-    {name: "megasloooooo",img: "images/megasloooooo.jpg"};
-    {name: "perros 2",img: "images/perros 2.jpg"};
-    {name: "perros 5 ",img: "images/perros 5.jpg"};
-    {name: "perros 7",img: "images/perros 7.jpg"};
-    {name: "perros 8",img: "images/perros 8.jpg"};];
+  const cardAd = [
+    {name: "Princess E-Girl Fashion (1)",img: "images/Princess E-Girl Fashion (1).jpg"},
+    {name: "megasloooooo",img: "images/megasloooooo.jpg"},
+    {name: "megasloooooo",img: "images/megasloooooo.jpg"},
+    {name: "perros 2",img: "images/perros 2.jpg"},
+    {name: "perros 5 ",img: "images/perros 5.jpg"},
+    {name: "perros 7",img: "images/perros 7.jpg"},
+    {name: "perros 8",img: "images/perros 8.jpg"},
+    {name: "Princess E-Girl Fashion (1)",img: "images/Princess E-Girl Fashion (1).jpg"},
+    {name: "megasloooooo",img: "images/megasloooooo.jpg"},
+    {name: "megasloooooo",img: "images/megasloooooo.jpg"},
+    {name: "perros 2",img: "images/perros 2.jpg"},
+    {name: "perros 5 ",img: "images/perros 5.jpg"},
+    {name: "perros 7",img: "images/perros 7.jpg"},
+    {name: "perros 8",img: "images/perros 8.jpg"}],
+    
 
+    
+    
     
   const cuadricula = document.querySelector(".cuadricula");
   const resultado = document.querySelector("#resultado");
@@ -20,7 +30,7 @@ document.addEventListener("DOMcontentLoaded", () => {
   function crearTablero (){
     for (let i = 0; i < cardAdj.length; i++) { // Loop que inicia en i=0, se incrementa de 1 en 1 (i++) y finaliza
                                                // cuando i=cardAdj.length, es decir, 12.
-      var carta = document.createElement('img'); // Crea elemento img cada vez que se ejecuta la función.
+var carta = document.createElement('img'); // Crea elemento img cada vez que se ejecuta la función.
       carta.setAttribute('src', 'images/reverso.png'); //Asignar nuevo atributo a cada carta
                                                        //igual a la imagen 'reverso'.
       carta.setAttribute('data-id', i); //Asignar como atributo a cada carta creada, el id=i.
