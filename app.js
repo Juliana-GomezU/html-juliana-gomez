3edf130e12abb68049ab47739f2af031fd2f7ddd
Juliana-GomezU <123768367+Juliana-GomezU@users.noreply.github.com>
2023-03-10 13:55:12 -0500

    lectura 4

--- app.js
+++ app.js
@@ -28,16 +28,25 @@ document.addEventListener("DOMcontentLoaded", () => {
 
 //----------------- lecture_03 ----------------------------------//  
   function crearTablero (){
-    for (let i = 0; i < cardAdj.length; i++) { // Loop que inicia en i=0, se incrementa de 1 en 1 (i++) y finaliza
-                                               // cuando i=cardAdj.length, es decir, 12.
-var carta = document.createElement('img'); // Crea elemento img cada vez que se ejecuta la función.
-      carta.setAttribute('src', 'images/reverso.png'); //Asignar nuevo atributo a cada carta
-                                                       //igual a la imagen 'reverso'.
-      carta.setAttribute('data-id', i); //Asignar como atributo a cada carta creada, el id=i.
-      carta.addEventListener('click', voltearCarta); //Detectar si la carta asignada recibe un click y ejecuta 
-                                                     //la función voltearCarta.
-      cuadricula.appendChild(carta); // Relaciona la carta creada con la variable cuadricula enlazada con la clase
-                                     // cuadricula de html para que se imprima la carta creada en el div de html.
-    }
-  }
-});
+    for (let i = 0; i < cardAdj.length; i++) { 
+                                               
+var carta = document.createElement('img'); 
+      carta.setAttribute('src', 'images/reverso.png'); 
+                                                     
+      carta.setAttribute('data-id', i); 
+      carta.addEventListener('click', voltearCarta); 
+                                                     
+      cuadricula.appendChild(carta); 
+      //----------------- lecture_04 ----------------------------------//
+      function voltearCarta(){
+        var cardid = this.getATTribute("data-id");
+        cartasEscogidas.push(cardadj(cardid).name),
+        cartasEscogidasid.push(cardadid);
+        this.setATTribute("src ,cardadj{cardid].img");
+        if(cartasEscogidas.length ===2) {
+          setTimeout(verificarPareja, 1000);
+
+        }
+      }
+      crearTablero(),
+    });
